d8393e6235e0fdb8110ffa70cdd3faf786cf432d
Kristina Uvarova <kduvarova@gmail.com>
2026-02-17 12:54:26 +0100

    mobile

---
+++ app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+
+  slidesPerView: 1.2,
+  spaceBetween: 8,
+
+  watchOverflow: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+  },
+
+});
