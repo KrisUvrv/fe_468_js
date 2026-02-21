30c21421dff1de0d98396cfe6adcbe6bc27ed2c8
Kristina Uvarova <kduvarova@gmail.com>
2026-02-21 13:18:21 +0100

    fixes

--- app.js
+++ app.js
@@ -1,51 +1,179 @@
+const LOGOS = [
+  {
+    src: './images/cat1.svg',
+    alt: 'brand logo 1',
+  },
+  {
+    src: './images/cat2.svg',
+    alt: 'brand logo 2',
+  },
+  {
+    src: './images/cat3.svg',
+    alt: 'brand logo 3',
+  },
+  {
+    src: './images/cat4.svg',
+    alt: 'brand logo 4',
+  },
+  {
+    src: './images/cat5.svg',
+    alt: 'brand logo 5',
+  },
+  {
+    src: './images/cat6.svg',
+    alt: 'brand logo 6',
+  },
+  {
+    src: './images/cat7.svg',
+    alt: 'brand logo 7',
+  },
+  {
+    src: './images/cat8.svg',
+    alt: 'brand logo 8',
+  },
+  {
+    src: './images/cat1.svg',
+    alt: 'brand logo 1',
+  },
+  {
+    src: './images/cat2.svg',
+    alt: 'brand logo 2',
+  },
+  {
+    src: './images/cat3.svg',
+    alt: 'brand logo 3',
+  },
+  {
+    src: './images/cat4.svg',
+    alt: 'brand logo 4',
+  },
+  {
+    src: './images/cat5.svg',
+    alt: 'brand logo 5',
+  },
+  {
+    src: './images/cat6.svg',
+    alt: 'brand logo 6',
+  },
+  {
+    src: './images/cat7.svg',
+    alt: 'brand logo 7',
+  },
+  {
+    src: './images/cat8.svg',
+    alt: 'brand logo 8',
+  },
+];
+
+const state = {
+  screenWidth: window.innerWidth,
+  isShowAll: false,
+  imagesCountCache: 0,
+};
+
+const calcSlidesPerView = () => {
+  const width = state.screenWidth;
+
+  const minW = 330;
+  const maxW = 768;
+
+  const minSlides = 1.2;
+  const maxSlides = 3;
+
+  const clampedWidth = Math.min(Math.max(width, minW), maxW);
+
+  const percent = (clampedWidth - minW) / (maxW - minW);
+  const value = minSlides + percent * (maxSlides - minSlides);
+
+  return Math.round(value / 0.01) * 0.01;
+}
+
+const swiperWrapper = document.getElementById('swiper-wrapper');
+
+const swiperSlides = LOGOS.map((logo) => {
+  const swiperSlide = document.createElement('div');
+  swiperSlide.className = 'swiper-slide';
+
+  const slide = document.createElement('img');
+  slide.src = logo.src;
+  slide.alt = logo.alt;
+
+  swiperSlide.appendChild(slide);
+  return swiperSlide;
+});
+swiperWrapper.append(...swiperSlides);
+
 const swiper = new Swiper('.swiper', {
-
-  slidesPerView: 1.2,
-
-  spaceBetween: 8,
-
+  slidesPerView: calcSlidesPerView(),
   watchOverflow: true,
-
-  speed: 1000, // плавнее
-
-  loop: true, // зациклены
+  speed: 1000,
+  loop: true,
 
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    dynamicBullets: true,
   },
-
 });
 
-function roundToStep(value, step) {
-  return Math.round(value / step) * step;
-}
-
-function calcSlidesPerView() {
-  const width = window.innerWidth; // ширина окна
-
-  const minW = 330; // Минимальная ширина диапазона
-  const maxW = 768; // Выше 768px — считаем, что достигли tablet.
-
-  const minSlides = 1.2; // Минимальное количество слайдов
-  const maxSlides = 3; // На планшете 3 слайда.
-
-  const clampedWidth = Math.min(Math.max(width, minW), maxW); // Ограничение диапазона; Если width меньше minW → берём minW; Если больше maxW → берём maxW. ✅ Итог: width всегда между 330 и 768.
-
-  const percent = (clampedWidth - minW) / (maxW - minW); // Находим процент диапазона 👉 насколько мы продвинулись от mobile к tablet
-  const value = minSlides + percent * (maxSlides - minSlides); // Получаем значение slidesPerView
-
-  return roundToStep(value, 0.01); // Округляем к шагу
-}
-
-function updateSwiper() {
+const renderMobile = () => {
   swiper.params.slidesPerView = calcSlidesPerView();
-  swiper.update();
+};
+
+const getVisibleLogosCount = () => {
+  if (state.isShowAll) return LOGOS.length;
+  if (state.screenWidth >= 1120) return 8;
+  if (state.screenWidth >= 768) return 6;
+
+  return LOGOS.length;
 }
 
-updateSwiper();
+const brandsContainer = document.getElementById('logos');
+const btn = document.getElementById('btn');
+const icon = document.getElementById('btnIcon');
+const text = document.getElementById('btnText');
 
-window.addEventListener('resize', updateSwiper);
-window.addEventListener('resize', () => {
-  console.log(window.innerWidth)
+const renderDesktop = () => {
+
+  const newCount = getVisibleLogosCount();
+  if (newCount === state.imagesCountCache) {
+    return;
+  }
+
+  brandsContainer.innerHTML = '';
+  const images = LOGOS.slice(0, newCount).map((logo) => {
+    const image = document.createElement("img");
+    image.src = logo.src;
+    image.alt = logo.alt;
+    return image;
+  });
+  brandsContainer.append(...images);
+  state.imagesCountCache = newCount;
+}
+
+const render = () => {
+  if (state.screenWidth < 768) {
+    renderMobile();
+  } else {
+    renderDesktop();
+  }
+};
+
+btn.addEventListener('click', () => {
+  state.isShowAll = !state.isShowAll;
+  if (state.isShowAll) {
+    icon.src = "./images/arrows_collapse.svg";
+    text.innerText = 'Скрыть';
+  } else {
+    icon.src = "./images/arrows_show.svg";
+    text.innerText = 'Показать все';
+  }
+  render();
 });
+
+window.addEventListener('resize', () => {
+  state.screenWidth = window.innerWidth;
+  render();
+});
+
+render();
